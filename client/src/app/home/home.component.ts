6ab3294013a777beacd53bbfc8302f94658e013b
AdityavardhanKoya <dummy@gmail.com>
2026-04-05 14:01:24 +0000

    homepage

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  // Variable to track scroll state
+  isScrolled = false;
+
   constructor() {}
+
+  // Listen to window scroll events
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    // If the user scrolls down more than 100 pixels, set isScrolled to true
+    this.isScrolled = window.scrollY > 100;
+  }
 }
